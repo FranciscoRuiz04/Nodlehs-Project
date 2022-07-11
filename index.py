09011675d9be2e79ff1c8296f0c1dd7fa9f8a452
Francisco Ruiz <franciscoruiz078@gmail.com>
2022-07-10 22:17:05 -0500

    Formatting code

--- index.py
+++ index.py
@@ -6,13 +6,14 @@ import geopandas as geopd
 import matplotlib.pyplot as plt
 import pandas as pd
 from dotenv import load_dotenv as env
-#-------------------------------------------------------------------#
+#-------------------------------------------------------------------
 
-env()   #Load variables from .env file
-data = pd.read_csv(os.getenv('rain_data'), encoding='utf8') #Read csv file
+env()  #Load variables from .env file
+data = pd.read_csv(os.getenv('rain_data'), encoding='utf8')  #Read csv file
 data.drop(data.index[42:], inplace=True)  #Drop blank rows
 
 ##########################  Functions   #############################
+
 
 def is_considered(row):
     nVals = nRecords = len(row) - 3
@@ -22,35 +23,37 @@ def is_considered(row):
     #Fetch just records with 80 percent of data or major
     if nVals >= nRecords * 0.8:
         return True
-#-------------------------------------------------------------------#
+
+
+#-------------------------------------------------------------------
 
 #Filter data
-for i,n in data.notna().iterrows():
+for i, n in data.notna().iterrows():
     if not is_considered(n):
         data.drop(index=i, inplace=True)
     data.fillna(0)
 
 #Generate a tuple with the total sum
 # instead of periodical records
-totalP = (row[2:5] + (sum(row[5:]),) for row in data.itertuples())
+totalP = (row[2:5] + (sum(row[5:]), ) for row in data.itertuples())
 
-heads = list(data.columns[1:4]) #Colnames without ID field
+heads = list(data.columns[1:4])  #Colnames without ID field
 heads.append("Total")
 #Create an enmpty outcome dataframe
 geoData = pd.DataFrame(columns=heads)
 
 #Fill geoData
-for i,rec in enumerate(totalP):
+for i, rec in enumerate(totalP):
     geoData.loc[i] = rec
 
 geo = geopd.read_file(os.getenv('myshp'))
 geoData = geo.merge(geoData, on='Nombre')
 geoSmn = geopd.read_file(os.getenv('smn'))
-geoData.geometry = geoData.geometry.buffer(5000,6)
+geoData.geometry = geoData.geometry.buffer(5000, 6)
 
 intersection = geopd.sjoin(geoData, geoSmn, how='left')
-means = (i[1]["Total"].mean() for i in intersection.groupby(["Nombre_left", "Pmean"]))
-
+means = (i[1]["Total"].mean()
+         for i in intersection.groupby(["Nombre_left", "Pmean"]))
 
 if __name__ == '__main__':
     print(next(means))
